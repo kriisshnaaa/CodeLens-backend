58f56cabd411b524b0d50513c588faa5ca5555fd
KRISHNA GUPTA <krishnaa941255@gmail.com>
2026-01-02 21:18:29 +0530

    mobile

--- src/config/session.js
+++ src/config/session.js
@@ -1,12 +1,15 @@
 const session = require("express-session");
 
 module.exports = session({
+  name: "connect.sid",
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
+  proxy: true,
   cookie: {
     httpOnly: true,
     secure: true,
-    sameSite: "none"
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
   }
 });
