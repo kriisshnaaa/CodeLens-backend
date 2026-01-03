20da0b8f41af6fda23812c93c7053a574c21b903
KRISHNA GUPTA <krishnaa941255@gmail.com>
2026-01-03 13:18:00 +0530

    my learning fix

--- src/routes/save.routes.js
+++ src/routes/save.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const isAuthenticated = require("../middlewares/isAuthenticated");
+const auth = require("../middlewares/auth");
 const saveController = require("../controllers/save.controller");
 
 const router = express.Router();
 
-router.post("/save", isAuthenticated, saveController.saveLearning);
-router.get("/my", isAuthenticated, saveController.getMyLearnings);
+router.post("/save", auth, saveController.saveLearning);
+router.get("/my", auth, saveController.getMyLearnings);
 
 module.exports = router;
